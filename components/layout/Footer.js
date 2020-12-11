05926625dc63f2cdd8d6c5f9a17d44771528db2e
Darcy Price <darcyelsing@gmail.com>
2020-12-11 20:15:10 +1100

    initial commit

---
+++ components/layout/Footer.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export default function Footer() {
+	return <StyledFooter>this is the footer</StyledFooter>;
+}
+
+const StyledFooter = styled.footer`
+	width: 100%;
+`;
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,43 @@
+import Header from '../components/layout/Header';
+import Footer from '../components/layout/Footer';
+
 export default function Home() {
-	return <div>hello</div>;
+	return (
+		<div>
+			<Header />
+			<main>
+				<section className='hero'>
+					<h1>Darcy Price</h1>
+					<h2>is getting things done at Impress Design</h2>
+				</section>
+				<section className='list-section'>
+					<div className='list-wrp'>
+						<h3>Currently</h3>
+						<ul>
+							<li>Full Stack Developer at Impress Design</li>
+							<li>XXX at YYY</li>
+							<li>AAA at BBB</li>
+						</ul>
+					</div>
+					<div className='list-wrp'>
+						<h3>Previously</h3>
+						<ul>
+							<li>Full Stack Developer at Impress Design</li>
+							<li>XXX at YYY</li>
+							<li>AAA at BBB</li>
+						</ul>
+					</div>
+					<div className='list-wrp'>
+						<h3>Interested In</h3>
+						<ul>
+							<li>Full Stack Developer at Impress Design</li>
+							<li>XXX at YYY</li>
+							<li>AAA at BBB</li>
+						</ul>
+					</div>
+				</section>
+			</main>
+			<Footer />
+		</div>
+	);
 }
