f5fd39fd20d3dc5b964e599b4330aee6e888e9d6
Darcy Price <darcyelsing@gmail.com>
2020-12-11 20:37:57 +1100

    removed navbar

--- src/components/navigation/desktopMenu.js
+++ src/components/navigation/desktopMenu.js
@@ -1,15 +1,13 @@
 import React from 'react';
 
-import NavItems from './navItems/navItems';
 import DarkModeToggle from '../UI/darkModeToggle';
 
 const DesktopMenu = ({ notOnePageSection }) => {
-  return notOnePageSection ? null : (
-    <>
-      <NavItems />
-      <DarkModeToggle />
-    </>
-  );
+	return notOnePageSection ? null : (
+		<>
+			<DarkModeToggle />
+		</>
+	);
 };
 
 export default DesktopMenu;
--- src/components/navigation/navbar.js
+++ src/components/navigation/navbar.js
@@ -9,90 +9,82 @@ import MobileMenu from './mobileMenu/mobileMenu';
 import LogoNavbar from '../../components/UI/logoNavbar';
 
 const StyledHeader = styled(animated.header)`
-  position: fixed;
-  width: 100%;
-  max-width: 100vw;
-  top: 0;
-  left: 0;
-  z-index: 20;
-  background: var(--navbar);
-  box-shadow: 0 0.5rem 2rem var(--shadow-color);
-  transition: background 0.2s ease-out;
+	position: fixed;
+	width: 100%;
+	max-width: 100vw;
+	top: 0;
+	left: 0;
+	z-index: 20;
+	background: var(--navbar);
+	box-shadow: 0 0.5rem 2rem var(--shadow-color);
+	transition: background 0.2s ease-out;
 `;
 
 const Wrapper = styled.div`
-  display: flex;
-  align-items: center;
-  overflow-x: hidden;
-  justify-content: space-between;
-  transition: all 0.2s ease-out;
-  user-select: none;
-  height: ${({ isMobile }) => (isMobile ? '6rem' : '7rem')};
+	display: flex;
+	align-items: center;
+	overflow-x: hidden;
+	justify-content: space-between;
+	transition: all 0.2s ease-out;
+	user-select: none;
+	height: ${({ isMobile }) => (isMobile ? '6rem' : '7rem')};
 
-  @media ${({ theme }) => theme.mediaQueries.small} {
-    position: relative;
-  }
+	@media ${({ theme }) => theme.mediaQueries.small} {
+		position: relative;
+	}
 `;
 
 const Navbar = ({ notOnePageSection }) => {
-  const [isMobile, setisMobile] = useState(null);
-  const [menuOpened, setMenuOpened] = useState(false);
+	const [isMobile, setisMobile] = useState(null);
+	const [menuOpened, setMenuOpened] = useState(false);
 
-  // Animation
-  const NavBarSpring = useSpring({
-    config: config.wobbly,
-    opacity: 1,
-    height: isMobile ? '6rem' : '7rem',
-    from: {
-      opacity: 0,
-      height: '0rem',
-    },
-  });
+	// Animation
+	const NavBarSpring = useSpring({
+		config: config.wobbly,
+		opacity: 1,
+		height: isMobile ? '6rem' : '7rem',
+		from: {
+			opacity: 0,
+			height: '0rem',
+		},
+	});
 
-  // Change navbar content accordingly
-  const changeMobile = () => {
-    window.matchMedia('(max-width: 37.5em)').matches
-      ? setisMobile(true)
-      : setisMobile(false);
-  };
+	// Change navbar content accordingly
+	const changeMobile = () => {
+		window.matchMedia('(max-width: 37.5em)').matches
+			? setisMobile(true)
+			: setisMobile(false);
+	};
 
-  // Event listener on resize, so when it change we check o remove desktop menu/mobile menu
-  // Better than CSS media query because we dont keep both DOM nodes
-  useEffect(() => {
-    changeMobile();
-    window.addEventListener('resize', changeMobile);
-    return () => window.removeEventListener('resize', changeMobile);
-  }, []);
+	// Event listener on resize, so when it change we check o remove desktop menu/mobile menu
+	// Better than CSS media query because we dont keep both DOM nodes
+	useEffect(() => {
+		changeMobile();
+		window.addEventListener('resize', changeMobile);
+		return () => window.removeEventListener('resize', changeMobile);
+	}, []);
 
-  return (
-    <StyledHeader style={NavBarSpring}>
-      <Contained>
-        <Wrapper isMobile={isMobile}>
-          <LogoNavbar
-            notOnePageSection={notOnePageSection}
-            setMenuOpened={setMenuOpened}
-          />
-          {isMobile ? (
-            <MobileMenu
-              notOnePageSection={notOnePageSection}
-              menuOpened={menuOpened}
-              setMenuOpened={setMenuOpened}
-            />
-          ) : (
-            <DesktopMenu notOnePageSection={notOnePageSection} />
-          )}
-        </Wrapper>
-      </Contained>
-    </StyledHeader>
-  );
+	return (
+		<StyledHeader style={NavBarSpring}>
+			<Contained>
+				<Wrapper>
+					<LogoNavbar
+						notOnePageSection={notOnePageSection}
+						setMenuOpened={setMenuOpened}
+					/>
+					<DesktopMenu notOnePageSection={notOnePageSection} />
+				</Wrapper>
+			</Contained>
+		</StyledHeader>
+	);
 };
 
 Navbar.propTypes = {
-  siteTitle: PropTypes.string,
+	siteTitle: PropTypes.string,
 };
 
 Navbar.defaultProps = {
-  siteTitle: ``,
+	siteTitle: ``,
 };
 
 export default Navbar;
